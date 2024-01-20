81205941dafc6ef15f120d000d2e49115c23305f
Jai <jai07rajput@gmail.com>
2024-01-20 11:56:20 +0530

    Variable-Assert

---
+++ var1.rs
@@ -0,0 +1,14 @@
+// fn main() {
+//    let x: i32 =  5;
+//    let y: i32 = 5;
+
+//    assert_eq!(x,5);
+//    println!("Succeeded");
+// }
+
+fn main() {
+   let mut x = 4;
+   x += 1;
+   assert_eq!(x,5);
+   println!("Succeeded");
+}
